a876707d60b44b9662a2b757c68a2d2743d897a4
Andrea Giorgetti <andreagiorgetti0@gmail.com>
2024-10-16 09:35:59 +0200

    Prova di hosting

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
-<?php
-// Connessione al database
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "prova.it";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // Connessione al database
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "prova.it";
 
-// Crea la connessione
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Controlla la connessione
-if ($conn->connect_error)
-    die("Connessione FALLITA: " . $conn->connect_error);
-else
-    header("Location: login.php");
-?>
-
-<!-- omgggg -->
+        // Crea la connessione
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        // Controlla la connessione
+        if ($conn->connect_error)
+            echo "<h1>Errore di connessione: '$conn->connect_error'</h1>";
+        else
+            header("Location: login.php");
+    ?>
+</body>
+</html>
